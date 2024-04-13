d653bc1915e69ab1c7261323d331842fb70fd3ad
nofarb <nofarb@gmail.com>
2024-04-13 11:15:02 -0500

    Update test_subtraction.py

--- test_subtraction.py
+++ test_subtraction.py
@@ -5,3 +5,7 @@ from calculator import Calculator
 def test_subtraction():
     calc = Calculator()
     assert calc.subtract(5, 3) == 2
+
+def test_subtraction2():
+    calc = Calculator()
+    assert calc.subtract(6, 3) == 3
